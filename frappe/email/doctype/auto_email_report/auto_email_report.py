e47bd01c2e55b60e84921c13fcf8e1cc1dd513cf
Ejaaz Khan <ejaaz@frappe.io>
2025-03-11 15:44:30 +0530

    refactor: remove unused import
    (cherry picked from commit 7d120575dbc73b30aa04bf78a1419353d5360c97)

--- frappe/email/doctype/auto_email_report/auto_email_report.py
+++ frappe/email/doctype/auto_email_report/auto_email_report.py
@@ -5,10 +5,6 @@ import calendar
 from datetime import timedelta
 
 import frappe
-import frappe.email
-import frappe.email.doctype
-import frappe.email.doctype.auto_email_report
-import frappe.email.doctype.auto_email_report.auto_email_report
 from frappe import _
 from frappe.desk.query_report import build_xlsx_data
 from frappe.model.document import Document
